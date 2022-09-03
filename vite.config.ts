d2dd330ca04bd6906aa9d72b449023f12f6c07d7
zhengyunxin <zhengyunxin@zuoyebang.com>
2022-09-03 23:27:18 +0800

    feat: 更新vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { resolve } from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: "./",
+  mode: "devlopment",
   resolve: {
     alias: {
-      '@': "./src"
+      '@': resolve(__dirname, "src")
     }
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 8080,
+    hmr: {
+      overlay: true,
+    },
+    proxy: {}
   }
 })
